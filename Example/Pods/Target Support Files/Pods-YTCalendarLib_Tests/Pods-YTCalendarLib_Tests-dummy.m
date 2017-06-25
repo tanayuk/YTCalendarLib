56c4e8ad70b2cbf111a22f1f54e32c02242667c6
Yuki Tanaka <tanaka@wedgebrothers.jp>
2017-06-25 16:20:20 +0900

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-YTCalendarLib_Tests/Pods-YTCalendarLib_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YTCalendarLib_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_YTCalendarLib_Tests
+@end
---
+++ Example/Pods/Target Support Files/YTCalendarLib/YTCalendarLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YTCalendarLib : NSObject
+@end
+@implementation PodsDummy_YTCalendarLib
+@end
